8b566b9b3d6fce2a2855d30c024d8e79e3daacb4
Kevin McNeil <kevin.michael.mcneil@gmail.com>
2022-03-21 17:19:56 -0700

    made some progress on the server but it is still not working

---
+++ benServer.js
@@ -0,0 +1,5 @@
+// var express = require('express');
+// const { append } = require('express/lib/response');
+// const app = express();
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, '../client/build')));
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join('descent-Journeys-in-the-dark', '..', 'public');
+const port = 3000;
+const publicPath = path.join(__dirname, '.', 'public');
 app.use(express.static(publicPath));
 
+
 app.get('*', (req, res) => {
+    console.log('made it');
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
